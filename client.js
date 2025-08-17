258d403efede757e5d5adf32a5b46f95b4cd8822
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-08-17 18:58:07 +0900

    rewrite client.js

--- client.js
+++ client.js
@@ -1,29 +1,52 @@
 import net from 'net';
 
 const SOCK_PATH = 'test.sock'; //? ./test.sock
+const delay = 1000;
+let client = null;
 
-const client = net.createConnection(SOCK_PATH);
-
-client.on('connect', () => {
-  console.log('connected');
-});
-client.on('data', (data) => {
-  console.log(data.toString());
-});
-client.on('end', () => {
-  console.log('disconnected.');
-  process.exit(0);
-});
-client.on('error', (err) => {
-  if (err.message.startsWith('connect ENOENT')) {
-    console.log("Cannot find socket (server.js isn't running?");
-    process.exit(1);
-  }
-  console.error('[err]', err.message);
-});
+let reconnecting = false;
+let reconnectTimer = null;
 
 let i = 0;
-setInterval(() => {
-  client.write(`Count is ${i}`);
-  i++;
-}, 1000);
+let msgInterval = null;
+
+const connect = () => {
+  if (reconnectTimer) {
+    clearTimeout(reconnectTimer);
+    reconnectTimer = null;
+  }
+
+  client = net.createConnection(SOCK_PATH);
+
+  client.on('connect', () => {
+    console.log('connected');
+    reconnecting = false;
+    msgInterval = setInterval(() => {
+      client.write(`Count is ${i}`);
+      i++;
+    }, 1000);
+  });
+  client.on('data', (data) => {
+    console.log(data.toString());
+  });
+  client.on('end', () => {
+    console.log('disconnected.');
+    clearInterval(msgInterval);
+    msgInterval = null;
+    reconnecting = true;
+    reconnectTimer = setTimeout(connect, delay);
+  });
+  client.on('error', (err) => {
+    if (err.message.startsWith('connect ENOENT')) {
+      console.log("Cannot find socket (server.js isn't running?");
+      if (reconnecting) {
+        reconnectTimer = setTimeout(connect, delay);
+        return;
+      }
+      process.exit(1);
+    }
+    console.error('[err]', err.message);
+  });
+};
+
+connect();
